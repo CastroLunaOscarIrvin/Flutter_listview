31135d039f9ce2a1df505e0334745c1dfb5942e6
OSCAR_IRVIN_CASTRO_LUNA <a.20308051280996@cbtis128.edu.mx>
2023-05-18 21:35:03 +0000

    WAAA

--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ListView2 extends StatelessWidget {
+  var juegos = ["Pou", "Fortnai", "Pacman", "Mortalcombat"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('List View tipo 2'),
         ),
-        body: ListView(
-          children: [],
+        body: ListView.separated(
+          // Obtener el tamaño de mi lista
+
+          itemCount: juegos.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(juegos[index]),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.black,
+            ),
+            onTap: () {
+              var seleccion = juegos[index];
+              print(seleccion);
+            },
+          ),
+          separatorBuilder: (_, __) => Divider(),
         ));
   }
 }
